9b1f54bc8b8753e5b480cd3aba6327a541e3d65d
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-05-27 17:49:44 +0300

    Collections

---
+++ 19_Exchange/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace _19_Exchange
+{
+    public delegate void OnCourseChange(int course);
+    class Exchange
+    {
+        private int course;
+        public event OnCourseChange ListTraders; 
+
+        public int Course
+        {
+            get { return course; }
+            set { 
+
+                course = value < 0 ? 0: value; 
+            }
+        }
+        public void GenerationCourse()
+        {
+            while (true)
+            {
+            Course = new Random().Next(25,45);
+            //Notification....
+            ListTraders?.Invoke(Course);
+                Thread.Sleep(1000);
+
+            }
+        }
+
+    }
+    class Trayder
+    {
+        public string Name { get; set; }
+        public int Money { get; set; }
+        public Trayder()
+        {
+            Name = "";
+            Money = 10000;
+        }
+        public Trayder(string name, int money)
+        {
+            Name = name;
+            Money = money;  
+        }
+        public void BuyOrSold(int course)
+        {
+            if(course < 30)
+            {
+                Console.WriteLine($"I bought currence for {course} ");
+                Money += course*100;
+                Console.WriteLine($"My money  {Money} ");
+            }
+            else if(course > 40)
+            {
+                Console.WriteLine($"I sold currence for {course} ");
+                Money -= course * 100;
+                Console.WriteLine($"My money  {Money} ");
+            }
+            else
+            {
+                Console.WriteLine("I'll wait.......");
+                Console.WriteLine($"My money  {Money} ");
+            }
+        }
+        public override string ToString()
+        {
+            return $"Name {Name} . Money {Money}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello";
+            string str2 = "hello";
+            string str3 = "hello";
+            string str4 = "hello";
+            string str5 = "hello";
+            string str6 = "hello";
+            string str7 = "hello";
+
+            Console.WriteLine(str.Equals(str2));
+            Console.WriteLine(str2.Equals(str3));
+            Console.WriteLine(str3.Equals(str4));
+            Exchange exchange = new Exchange();
+
+            Trayder[] trayders = new Trayder[]
+            {
+                 new Trayder{ Name = "Ira",Money = 1000000},
+            new Trayder  {Name = "Vlad", Money = 20000},
+            new Trayder{ Name = "Vova", Money = 3000},
+            new Trayder{ Name = "Nina",Money = 999999}
+            };
+
+            foreach (var tr in trayders)
+            {
+                exchange.ListTraders += tr.BuyOrSold;
+            }
+
+            //exchange.GenerationCourse();
+
+            Console.WriteLine($"Поколение объекта: {GC.GetGeneration(trayders)}");
+            Console.WriteLine($"Занято памяти(байт): {GC.GetTotalMemory(false)}");
+
+        }
+    }
+}
---
+++ 22_Dictionary/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _22_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> countries = new Dictionary<string, string>();
+
+            countries.Add("UA", "Ukraine");
+            countries.Add("GB", "Great Britain");
+            countries.Add("USA", "United Sates");
+            countries.Add("CH", "China");
+            countries.Add("PL", "Poland");
+
+            foreach (KeyValuePair<string, string> keyValue in countries)
+            {
+                Console.WriteLine(keyValue.Key + "  " + keyValue.Value);
+            }
+            Console.WriteLine();
+            string country = countries["USA"];
+            Console.WriteLine("Find by key : " + country);
+
+            countries["IN"] = "India";
+            countries.Remove("CH");
+            foreach (KeyValuePair<string, string> keyValue in countries)
+            {
+                Console.WriteLine(keyValue.Key + "  " + keyValue.Value);
+            }
+            Console.WriteLine("-----------------------------------");
+            Dictionary<char, Person> people = new Dictionary<char, Person>();
+            people.Add('O', new Person() { Name = "Olena" });
+            people.Add('I', new Person() { Name = "Irina" });
+            //people.Add('O', new Person(){ Name = "Oksana"});error
+            people.Add('M', new Person() { Name = "Mukola" });
+            people.Add('V', new Person() { Name = "Vova" });
+
+            people['O'] = new Person() { Name = "Oksana" };
+            people['D'] = new Person() { Name = "Dmutro" };
+
+            foreach (KeyValuePair<char, Person> keyValue in people)
+            {
+                Console.WriteLine(keyValue.Key + "  " + keyValue.Value.Name);
+            }
+
+            if (people.ContainsKey('R'))
+            {
+                people['R'].Name = "Roman";
+            }
+            else
+            {
+                Console.WriteLine("Collection does not contain this key");
+            }
+
+            foreach (char c in people.Keys)
+            {
+                Console.WriteLine(c);
+            }
+            foreach (Person p in people.Values)
+            {
+                Console.WriteLine(p.Name);
+            }
+            people.Add('T', new Person() { Name = "Tamara" });
+            people['S'] = new Person() { Name = "Sergiy" };
+
+            foreach (KeyValuePair<char, Person> keyValue in people)
+            {
+                Console.WriteLine(keyValue.Key + "  " + keyValue.Value.Name);
+            }
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
